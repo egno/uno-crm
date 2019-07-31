1faeca27914c8e107341e57ea838c8aa1065f8a5
Natalya Norboeva <divonata@gmail.com>
2019-07-31 13:37:30 +0700

    add dependencies; merge eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    commonjs: true,
+    es6: true,
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module',
+    parser: 'babel-eslint',
+    ecmaVersion: 2018
   },
   extends: [
     '@nuxtjs',
@@ -19,5 +26,21 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'import/no-unresolved': 0,
+    'import/no-unassigned-import': 0,
+    'semi': ['error', 'never'],
+    'no-console': 'off',
+    "space-before-function-paren": [
+      "error",
+      {
+        "anonymous": "always",
+        "named": "always",
+        "asyncArrow": "always"
+      }
+    ],
+    'vue/max-attributes-per-line': 0,
+    'vue/no-parsing-error': [2, {
+      "invalid-first-character-of-tag-name": false
+    }]
   }
 }
