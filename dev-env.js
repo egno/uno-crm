939ca0eaeb18c49fc9a1421657f880047cbdb942
Natalya Norboeva <divonata@gmail.com>
2019-08-23 17:42:47 +0700

    change dev env

--- dev-env.js
+++ dev-env.js
@@ -1,9 +1,9 @@
 /* eslint-disable quotes */
 export default {
-  "VUE_APP_API": "https://dev.unosalon.ml/api/v1/",
-  "VUE_APP_IMAGES": "https://dev.unosalon.ml/images/",
-  "VUE_APP_UPLOAD": "https://dev.unosalon.ml/uploads/",
-  "VUE_APP_BILLING_API": "https://dev.unosalon.ml/api/billing/",
+  "VUE_APP_API": "https://crm.unosalon.ml/api/v1/",
+  "VUE_APP_IMAGES": "https://crm.unosalon.ml/images/",
+  "VUE_APP_UPLOAD": "https://crm.unosalon.ml/uploads/",
+  "VUE_APP_BILLING_API": "https://crm.unosalon.ml/api/billing/",
   "VUE_APP_WIDGET_ADDRESS": "https://widget.unosalon.ml/",
   "VUE_APP_YANDEX_ID": 0,
   "VUE_APP_DEV_PAYMASTER_ID": "0c4c55fd-2d06-416c-9f6f-54806100c501",
